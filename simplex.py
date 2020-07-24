9169715795e95687c7ab21b11197965d4ea9bf9f
PanJixiang <narfnas@163.com>
2020-07-24 19:10:54 +0800

    not started

--- simplex.py
+++ simplex.py
@@ -1,5 +1,16 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
 
-# 单纯型法的 Python 实现
+
+class LinearProgramming:
+    '线性规划'
+
+    def __init__(self, array_a, array_b, array_c):
+        self.array_a = array_a
+        self.array_b = array_b
+        self.array_c = array_c
+
+    def simplex(self):
+        self.
